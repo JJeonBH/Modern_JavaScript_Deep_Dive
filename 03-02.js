d13cc02b368f9ca21e11f87292809ecffd33a8f8
JJeonBH <wwjjss5386@gmail.com>
2024-01-20 00:42:53 +0900

    클라이언트 사이드 Wep API

--- 03-02.js
+++ 03-02.js
@@ -1,3 +1,6 @@
 const arr = [1, 2, 3];
 
-arr.forEach(console.log);
+arr.forEach(alert);
+// alert 함수는 브라우저에서만 동작하는 클라이언트 사이드 Web API다.
+// alert 함수는 브라우저 환경에서만 유효하다.
+// 클라이언트 사이드 Web API는 Node.js 환경에서 실행할 수 없다.
